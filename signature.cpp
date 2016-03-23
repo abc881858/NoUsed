a8a2e375bf8d7fe1b44914c99a7aba8027111610
abc881858 <abc8333@gmail.com>
2016-03-23 15:09:05 +0800

    Update signature.cpp

--- signature.cpp
+++ signature.cpp
@@ -1,29 +1,38 @@
 #include "signature.h"
+#include <stdint.h>
+#include <string>
+#include <sstream>
+#include <algorithm>
 
 Signature::Signature(QString pattern, QString mask): pattern(pattern), mask(mask)
 {
 
 }
 
-Signature::Signature(QString idapattern)
+Signature::Signature(QString str)
 {
-//    idapattern.erase(std::remove(idapattern.begin(), idapattern.end(), ' '), idapattern.end());
-//    for (auto it = idapattern.cbegin(); it != idapattern.cend();)
-//    {
-//        if (*it == wildcard)
-//        {
-//            pattern += '\0';
-//            mask += wildcard;
-//            it++;
-//            continue;
-//        }
-//        QString cur(it, it + 2);
-//        std::stringstream converter(cur);
-//        uint16_t current = 0;
-//        converter >> std::hex >> current >> std::dec;
-//        char real_current = static_cast<char>(current);
-//        pattern += real_current;
-//        mask += 'x';
-//        it += 2;
-//    }
+    QString r = str.remove(QChar(' '));
+    std::string idapattern = r.toStdString();
+
+    for (auto it = idapattern.cbegin(); it != idapattern.cend();)
+    {
+        if (*it == wildcard)
+        {
+            pattern += '\0';
+            mask += wildcard;
+            it++;
+            continue;
+        }
+        std::string cur(it, it + 2);
+        std::stringstream converter(cur);
+        uint16_t current = 0;
+        converter >> std::hex >> current >> std::dec;
+        char real_current = static_cast<char>(current);
+        pattern += real_current;
+        mask += 'x';
+        it += 2;
+
+        qDebug() << "pattern= " << pattern;
+        qDebug() << "mask= " << mask;
+    }
 }
